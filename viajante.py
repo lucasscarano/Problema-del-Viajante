5b596ff2433688b598d2d99c5ca0d7ada5225776
Juan Ignacio Elias <Juanignacioelias@hotmail.com>
2020-09-10 10:03:47 -0300

    Converted to .py file and added Excel to directory

---
+++ viajante.py
@@ -0,0 +1,66 @@
+import numpy as np
+import random
+import matplotlib as plot
+import pandas as pd
+
+tam_poblacion = 50
+cant_provincias = 23
+chances_crossover = 0.75
+array_poblacion = [0] * tam_poblacion
+nombre_capitales = [0] * cant_provincias
+array_fitness = [0] * tam_poblacion
+
+distancias = pd.read_excel('TablaCapitales.xlsx')
+distancias = distancias.to_numpy()
+
+for k in range(0, 23):
+    nombre_capitales[k] = distancias[k][0]
+
+distancias = pd.read_excel('TablaCapitales.xlsx', index_col=0)
+distancias = distancias.to_numpy()
+
+
+def poblacion_inicial():
+    for i in range(0, tam_poblacion):
+        cromosoma = random.sample(range(cant_provincias), cant_provincias)
+        array_poblacion[i] = cromosoma
+
+
+def ciclico(padre1, padre2):
+    hijo = [None] * cant_provincias
+    j = 0
+    hijo[j] = padre1[j]
+    flag = True
+    while flag:
+        aux_j = padre2[j]
+        j = padre1.index(aux_j)
+        if hijo[j] is None:
+            hijo[j] = padre1[j]
+        else:
+            flag = False
+    for j in range(1, cant_provincias):
+        if hijo[j] is None:
+            hijo[j] = padre2[j]
+
+    return hijo
+
+
+def crossover():
+    for i in range(0, tam_poblacion, 2):
+        cros = random.random()
+        if cros < chances_crossover:
+            padre1 = array_poblacion[i]
+            padre2 = array_poblacion[i + 1]
+            array_poblacion[i] = ciclico(padre1, padre2)
+            array_poblacion[i + 1] = ciclico(padre2, padre1)
+
+
+def fitness():
+    aux_fitness = 0
+    for i in range(0, cant_provincias):
+        aux_fitness += distancias[array_poblacion[i]][array_poblacion[i + 1]]
+
+
+poblacion_inicial()
+crossover()
+fitness()
